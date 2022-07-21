bfbe8aed2a83dc4aa7b56b554e524b44db300c3a
Arefinrumi <76535072+Arefinrumi@users.noreply.github.com>
2022-07-21 18:52:58 +0600

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello {name}!</h3>" \
+    html = "<h3>Arefin {name}!</h3>" \
            "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), visits=visits)
+    return html.format(name=os.getenv("NAME", "Rumi"), visits=visits)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
